2f24f48088c484d6d8fcb640a47401c39118e249
chennan <CHENNAN177@pingan.com.cn>
2016-09-26 13:25:07 +0800

    x

--- src/main/java/org/lc/core/LcPinyinSearchSegmenter.java
+++ src/main/java/org/lc/core/LcPinyinSearchSegmenter.java
@@ -23,13 +23,10 @@ public class LcPinyinSearchSegmenter extends AbstractPinyinSegmenter {
         char ch = token.charAt(0);
         if (CharacterUtil.isLetter(ch)) {
             List<String> pinyinList = segReverse(token, MAX_LENGTH);
-            lexemeList.add(new Lexeme(getOffset(), token.length(), token.length(), 1, CharacterUtil.CHAR_ENGLISH, token));
-            if (pinyinList.size() > 1) {
-                int pinyinIdx = 0;
+            if (pinyinList.size() > 0) {
                 for (String pinyinItem : pinyinList) {
-                    lexemeList.add(new Lexeme(getOffset(), pinyinItem.length(), pinyinItem.length(), Math.min(1, pinyinIdx), CharacterUtil.CHAR_ENGLISH, pinyinItem));
+                    lexemeList.add(new Lexeme(getOffset(), pinyinItem.length(), pinyinItem.length(), 1, CharacterUtil.CHAR_ENGLISH, pinyinItem));
                     incrementOffset(pinyinItem.length());
-                    pinyinIdx++;
                 }
             }
             incrementOffset(token.length());
